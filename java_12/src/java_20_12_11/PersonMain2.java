f9c71c06630b37bc1c7ed6e14d04005c27b79dc6
castle1 <dlf5401@naver.com>
2020-12-12 20:12:25 +0900

    20-12-12

--- java_12/src/java_20_12_11/PersonMain2.java
+++ java_12/src/java_20_12_11/PersonMain2.java
@@ -16,9 +16,12 @@ public class PersonMain2 {
 
 		while (tf) {
 			sc = new Scanner(System.in);
-			enter = sc.nextLine();
+			enter = sc.nextLine(); 
+			//\n을 포함하는 한 라인을 읽고 \n 을 버린 나머지만 리턴
 
 			if (enter.trim().equals("")) {
+				//\n을 버린 나머지를 리턴받아 가장 앞과 가장 뒤 공백을 제거한 enter가
+				//"" 과 같냐?
 				sh.gameStart();
 
 				if (sh.br() == true) {
